0385b003e07c594f5f77e2cfded419612828574f
Sam Grogan <sam.grogan@bottlerocketstudios.com>
2021-06-15 18:22:20 -0500

    epoch update

--- train.py
+++ train.py
@@ -2,4 +2,4 @@ from DCGAN import DCGAN
 
 if __name__ == '__main__':
     dcgan = DCGAN()
-    dcgan.train(epochs=9000, batch_size=1, save_interval=1, data_dir="map_images")
+    dcgan.train(epochs=40000, batch_size=1, save_interval=1, data_dir="map_images")
